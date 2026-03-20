5d28cfb458a58eecd7190bb8ca6bb64ed23387b3
djoni1vincent <djoni8vincent@gmail.com>
2026-03-20 19:48:24 +0100

    fead: Add feature to post to update/delete them

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,11 +1,10 @@
 import os
 import secrets
 
-from flask import flash, redirect, render_template, request, url_for
+from flask import abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user, logout_user
 from flask_login.utils import login_required
 from PIL import Image
-from sqlalchemy.sql.functions import random
 
 from flaskblog import app, bcrypt, db
 
@@ -146,3 +145,40 @@ def new_post():
         return redirect(url_for('home'))
 
     return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/update/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form)
+
+
+@app.route('/post/delete/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
