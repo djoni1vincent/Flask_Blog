d218b37a437a49a4994f5e3e4f8ef3606dc06c6f
djoni1vincent <djoni8vincent@gmail.com>
2026-03-24 22:22:08 +0100

    refactor: Add secret key to .env

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,18 @@
 import os
 
+import markdown
+from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'd5bc44df5c2bd239c70784955dff0079'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
@@ -18,5 +24,19 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+page_down = PageDown(app)
+
+
+@app.template_filter('markdown')
+def render_markdown(text):
+    return markdown.markdown(text, extensions=['extra', 'codehilite'])
+
 
 from flaskblog import routes
