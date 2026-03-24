593bad90eab0f3636e0d082174d5a3ebbb3e1c30
djoni1vincent <djoni8vincent@gmail.com>
2026-03-24 22:07:37 +0100

    fead: Add 2 new routes for requesting and reseting token, for changing password.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -207,4 +207,38 @@ def user_post(username):
         'user_post.html', posts=posts, title='User Posts', user=user
     )
 
+def send_reset_email():
+    
+    
+    
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.login.data).first()
+        send_reset_email()
+        flash("An email with instructions to reset your password.", 'info')
+        redirect(url_for('login'))
+    return render_template(
+        'reset_request.html', form=form, title='Reset Password'
+    )
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+
+    if user is None:
+        flash('That is an invalid or expired token.', 'warning')
+        return redirect(url_for('reset_request'))
+
+    form = ResetPasswordForm
+
+    return render_template(
+        'reset_token.html', title='Reset Password', form=form
+    )
