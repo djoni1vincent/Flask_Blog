c5c8e36f81ab885224a59140fc9cdd17cdf16b36
djoni1vincent <djoni8vincent@gmail.com>
2026-04-04 20:29:07 +0200

    fead: Add errors handlers
    If you get 404, 403, 500 errors, you get to another page with
    description.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-import markdown
 from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
@@ -7,31 +6,48 @@ from flask_mail import Mail
 from flask_pagedown import PageDown
 from flask_sqlalchemy import SQLAlchemy
 
+from flaskblog.config import Config
+
 load_dotenv()
 
 
-app = Flask(__name__)
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-page_down = PageDown(app)
+page_down = PageDown()
 
 
-@app.template_filter('markdown')
 def render_markdown(text):
+    import markdown
+
     return markdown.markdown(text, extensions=['extra', 'codehilite'])
 
 
-from flaskblog.main.routes import main
-from flaskblog.posts.routes import posts
-from flaskblog.users.routes import users
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(posts)
-app.register_blueprint(main)
-app.register_blueprint(users)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    page_down.init_app(app)
+
+    app.add_template_filter(render_markdown, 'markdown')
+
+    from flaskblog.errors.handlers import errors
+    from flaskblog.main.routes import main
+    from flaskblog.posts.routes import posts
+    from flaskblog.users.routes import users
+
+    app.register_blueprint(errors)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+
+    return app
